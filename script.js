d4e3d99fbf20bcc0bb3f93cf9c3dfbc146cf6e24
prabhuprakash <prabhuprakash369@gmail.com>
2024-12-02 16:02:18 +0530

    commit

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 
 async function getText(text) {
-    let p = "http://www.omdbapi.com/?apikey=f4741886&t="+text;
-    let x = await fetch(p);
-    let y = JSON.parse(await x.text());
-    let l="";
-    console.log(typeof(y));
-    for(var k in y){
-        //console.log(k + ': ' + y[k] +'<br>');
-        if(k!='Ratings' && k!='Plot' && k!='Poster')
-        l +=`${k}  ': '  ${y[k]} '<br>'`;
-    }
-    document.getElementById("p1").innerHTML=l;
+    let apiCall = "http://www.omdbapi.com/?apikey=f4741886&s="+text;
+    let recievedData = await fetch(apiCall);
+    const data = JSON.parse(await recievedData.text());
+    let t="";
+    data.Search.forEach(movie=>{
+      t+=`<div id="class2"><img src="${movie.Poster}"> <br>Title : ${movie.Title} <br> Year : ${movie.Year} <br></div>`;
+    })
+    document.getElementById("p1").innerHTML=t;
   }
