f36936521f5a9a623d712ef45c28300f26b7a365
prabhuprakash <prabhuprakash369@gmail.com>
2025-01-30 12:07:39 +0530

    created carmanagement project

---
+++ carsManagement/src/test/java/com/carsmanagement/demo/CarsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carsmanagement.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
